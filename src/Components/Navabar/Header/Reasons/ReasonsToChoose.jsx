a6dbe83bbc391484945b620af17750ae78e68c42
Zeeshan Ali Zafar <zeeshanalizafar53@gmail.com>
2026-04-17 13:54:03 +0500

    Core Resposiveness

--- src/Components/Navabar/Header/Reasons/ReasonsToChoose.jsx
+++ src/Components/Navabar/Header/Reasons/ReasonsToChoose.jsx
@@ -5,14 +5,14 @@ function ReasonsToChoose() {
     <div className='mb-5'>
 
         <div className='bg-white shadow-md font-bold p-4 text-[25px] w-[100%] flex justify-center Main-Heading '>
-            "Why Choose Mindful Machines?"
+            "Why Mindful Machines?"
         </div>
         <div className='flex  pt-5 justify-center items-center'>
-            <div className='flex items-center justify-center w-[80%] '>
+            <div className='flex items-center justify-center w-[80%] Mobile-Reason'>
             <div className='w-[50%] flex justify-center font-bold items-center Image-Why-Home overflow-hidden '>
                 <img className='w-[80%] Image-Why-Home' src ="https://media.istockphoto.com/id/1495819409/photo/digital-mind-brain-artificial-intelligence-concept.jpg?s=1024x1024&w=is&k=20&c=AOs5JEz8sdE2wwrZmRSSd0dL8qj00ebD8tNY_AWb1yk=" />
             </div>
-        <div className=' w-[50%] font-bold p-4 text-[25px] flex flex-col gap-3 justify-center '>
+        <div className=' w-[50%] Mobile-text-Reason font-bold p-4 text-[25px] flex flex-col gap-3 justify-center '>
 
          <div>Intelligence That Learns!</div>
          <div className='text-[18px]'>"Our machines don't just follow instructions, they understand context. Powered by advanced neural networks, our AI adapts in real-time to provide personalized health insights and precision industrial movements."</div>
@@ -20,9 +20,9 @@ function ReasonsToChoose() {
 </div>
 </div>
 
-         <div className='flex  pt-5 justify-center items-center'>
-            <div className='flex items-center justify-center w-[80%] '>
-                 <div className=' w-[50%] font-bold p-4 text-[25px] flex flex-col gap-3 justify-center '>
+         <div className='flex  pt-5 justify-center items-center '>
+            <div className='flex items-center justify-center w-[80%] Mobile-Reason'>
+                 <div className=' w-[50%] Mobile-text-Reason font-bold p-4 text-[25px] flex flex-col gap-3 justify-center '>
 
          <div>Seamless Integration.</div>
          <div className='text-[18px]'>"Technology that feels like second nature. From wearable health trackers to wellness companions, our devices are designed to enhance your physical well-being while remaining invisible in your daily routine."</div>
@@ -36,12 +36,12 @@ src ="https://media.istockphoto.com/id/1159288175/photo/young-woman-checking-the
 </div>
 </div>
       <div className='flex  pt-5 justify-center items-center'>
-            <div className='flex items-center justify-center w-[80%] '>
+            <div className='flex items-center justify-center w-[80%] Mobile-Reason'>
             <div className='w-[50%] flex justify-center font-bold items-center Image-Why-Home overflow-hidden '>
                 <img className='w-[80%] Image-Why-Home'
  src ="https://images.pexels.com/photos/11951215/pexels-photo-11951215.jpeg" />
             </div>
-        <div className=' w-[50%] font-bold p-4 text-[25px] flex flex-col gap-3 justify-center '>
+        <div className=' w-[50%] Mobile-text-Reason font-bold p-4 text-[25px] flex flex-col gap-3 justify-center '>
 
          <div>Precision at Scale!</div>
          <div className='text-[18px]'>"Engineered for the demands of tomorrow. Our industrial robots and autonomous drones offer unmatched precision and durability, reducing downtime and optimizing workflows in the most challenging environments"</div>
@@ -49,8 +49,8 @@ src ="https://media.istockphoto.com/id/1159288175/photo/young-woman-checking-the
 </div>
 </div>
       <div className='flex  pt-5 justify-center items-center'>
-            <div className='flex items-center justify-center w-[80%] '>
-                  <div className='w-[50%]   font-bold p-4 text-[25px] flex flex-col gap-3 justify-center '>
+            <div className='flex items-center justify-center w-[80%] Mobile-Reason'>
+                  <div className='w-[50%] Mobile-text-Reason  font-bold p-4 text-[25px] flex flex-col gap-3 justify-center '>
 
          <div>The Ethical Future.</div>
          <div className='text-[18px]'>"We build machines with a conscience. Our mission is to bridge the gap between human potential and mechanical efficiency, ensuring a future where AI serves to protect, assist, and empower humanity."</div>
